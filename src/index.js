447e4f15533e147fe5b1ab4c66adff4ef7f61f72
JPrevilon <joshuaprevilon13@gmail.com>
2026-01-22 17:20:30 -0500

    Complete task lister form submit and append tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = event.target.querySelector('input[name="new-task-description"]');
+    const task = taskInput.value;
+
+    buildToDo(task);
+
+    event.target.reset();
+  });
 });
+
+function buildToDo(task) {
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  const taskList = document.querySelector("#tasks");
+  taskList.appendChild(li);
+}
